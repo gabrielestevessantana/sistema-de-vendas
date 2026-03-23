4d23d4c3bf4a31389fa3256102e075d86a6d5866
Gabriel Esteves Santana <esteves@MacBook-Air-de-Gabriel.local>
2026-03-23 15:46:24 -0300

    Implementa CRUD do sistema de vendas

---
+++ src/main/java/com/projeto/sistema/controllers/principalController.java
@@ -0,0 +1,14 @@
+package com.projeto.sistema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class principalController {
+	
+	@GetMapping("/administrativo")
+	public String acessarPrincipal() {
+		return "administrativo/home";
+	}
+
+}
