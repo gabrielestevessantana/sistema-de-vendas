63fa07c45d7b54c5dda361693bb5447305ad6057
Gabriel Esteves Santana <esteves@MacBook-Air-de-Gabriel.local>
2026-03-30 19:54:28 -0300

    Implanta cadastro e listagem de Produtos

---
+++ src/main/java/com/projeto/sistema/models/Produto.java
@@ -0,0 +1,88 @@
+package com.projeto.sistema.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto implements Serializable {
+
+	 private static final long serialVersionUID = 1L;
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private Long id;
+	    
+	    private String nome;
+	    private String codigoBarras;
+	    private String unidadeMedida;
+	    private double estoque;
+	    private double precoCusto;
+	    private double precoVenda;
+	    private double lucro;
+	    private double margemLucro;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getCodigoBarras() {
+			return codigoBarras;
+		}
+		public void setCodigoBarras(String codigoBarras) {
+			this.codigoBarras = codigoBarras;
+		}
+		public double getEstoque() {
+			return estoque;
+		}
+		public void setEstoque(double estoque) {
+			this.estoque = estoque;
+		}
+		
+		
+		public String getUnidadeMedida() {
+			return unidadeMedida;
+		}
+		public void setUnidadeMedida(String unidadeMedida) {
+			this.unidadeMedida = unidadeMedida;
+		}
+		public static long getSerialversionuid() {
+			return serialVersionUID;
+		}
+		public double getPrecoCusto() {
+			return precoCusto;
+		}
+		public void setPrecoCusto(double precoCusto) {
+			this.precoCusto = precoCusto;
+		}
+		public double getPrecoVenda() {
+			return precoVenda;
+		}
+		public void setPrecoVenda(double precoVenda) {
+			this.precoVenda = precoVenda;
+		}
+		public double getLucro() {
+			return lucro;
+		}
+		public void setLucro(double lucro) {
+			this.lucro = lucro;
+		}
+		public double getMargemLucro() {
+			return margemLucro;
+		}
+		public void setMargemLucro(double margemLucro) {
+			this.margemLucro = margemLucro;
+		}
+	    
+	    
+}
